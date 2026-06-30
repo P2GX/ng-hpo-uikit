6eeef514503947ee71b8445e8fe2ec3005574883
Peter N. Robinson <peter.robinson@bih-charite.de>
2026-06-30 17:20:59 +0200

    removing obsolete stories files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/lib/**/*.mdx",
+    "../src/lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-a11y",
