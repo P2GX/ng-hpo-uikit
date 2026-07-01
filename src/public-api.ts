649570c07702be134319a9efe62a7b3fe1e3ebf1
Peter N. Robinson <peter.robinson@bih-charite.de>
2026-07-01 15:41:55 +0200

    Updates

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,17 +3,19 @@
  */
 
 export * from './lib/ng-hpo-uikit';
+
 export * from './lib/footer/footer.component';
 export * from './lib/help-button/help-button.component';
-export * from './lib/orcid-dialog/orcid-dialog'
-export * from './lib/load-ontology/load-ontology.component';
-export * from './lib/phenopacket-loader/phenopacket-loader.component'
-export * from './lib/hpo-annotation-polisher-row/hpo-annotation-polisher.component'
+export * from './lib/hpo-annotation-polisher-row/hpo-annotation-polisher.component';
 export { PolishedHpoAnnotation } from './lib/hpo-annotation-polisher-row/hpo-annotation-polisher.interface';
-export * from './lib/hpo-onset-selector/hpo-onset-selector.component'
-export * from './lib/hpo-modifier-menu/hpo-modifier-menu.component'
-
-
+export * from './lib/hpo-onset-selector/hpo-onset-selector.component';
+export * from './lib/hpo-mining/hpo-mining.component';
+export * from './lib/hpo-modifier-menu/hpo-modifier-menu.component';
+export * from './lib/load-ontology/load-ontology.component';
+export * from './lib/orcid-dialog/orcid-dialog';
+export * from './lib/phenopacket-loader/phenopacket-loader.component';
+export * from './lib/models/fenominal-models';
+export * from './lib/models/ontology-dto';
 
 
 /* services */
